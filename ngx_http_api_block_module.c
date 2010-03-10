8a5ad06dff3965496eb1372f0ff0d434b4adb01d
Dan Loewenherz <dloewenherz@gmail.com>
2010-03-09 18:27:50 -0800

    random commit

--- ngx_http_api_block_module.c
+++ ngx_http_api_block_module.c
@@ -11,21 +11,25 @@
 #include <libmemcached/memcached.h>
 
 typedef struct {
-    ngx_uint_t  methods;
-    ngx_uint_t  access;
-    ngx_uint_t  min_delete_depth;
-    ngx_flag_t  create_full_put_path;
-} ngx_http_dav_loc_conf_t;
+    ngx_flag_t log;
+} ngx_http_api_block_loc_conf_t;
 
+static ngx_int_t ngx_http_api_block_handler(ngx_http_request_t *r);
 static char *ngx_http_api_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+
+static ngx_http_api_block_header_filter_pt ngx_http_next_body_filter;
 
 static ngx_command_t ngx_http_api_block_commands[] = {
     {
 			ngx_string("api_block"),
       NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_FLAG,
-      ngx_http_api_block,
-      0,
-      0,
+			/* access this variable through 
+				rlcf = ngx_http_get_module_loc_conf(r, ngx_http_rewrite_module);
+				rlcf->log
+				*/
+      ngx_conf_set_flag_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_rewrite_loc_conf_t, log),
       NULL
 		},
 
@@ -34,7 +38,7 @@ static ngx_command_t ngx_http_api_block_commands[] = {
 
 static ngx_http_module_t ngx_http_api_block_module_ctx = {
 	NULL,                                  /* preconfiguration */
-	NULL,                                  /* postconfiguration */
+	ngx_http_api_block_init,							 /* postconfiguration */
 
 	NULL,                                  /* create main configuration */
 	NULL,                                  /* init main configuration */
@@ -61,7 +65,8 @@ ngx_module_t  ngx_http_api_block_module = {
     NGX_MODULE_V1_PADDING
 };
 
-static ngx_int_t ngx_http_api_block_handler(ngx_http_request_t *r) {
+static ngx_int_t
+ngx_http_api_block_body_filter(ngx_http_request_t *r, ngx_chain_link *in) {
     ngx_int_t rc;
     ngx_buf_t *b;
     ngx_chain_t out;
@@ -91,6 +96,7 @@ static ngx_int_t ngx_http_api_block_handler(ngx_http_request_t *r) {
 			sprintf(value, "%d", i);
 		  rc_m = memcached_set(ab_memcache, remote_addr_val, remote_addr_len, 
 					value, sizeof(value), (time_t)i, (uint32_t)0);
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
 		}
 		else {
 			value = "MISS";
@@ -98,7 +104,6 @@ static ngx_int_t ngx_http_api_block_handler(ngx_http_request_t *r) {
 					"1", sizeof("1"), (time_t)1, (uint32_t)0);
 		}
 		size = sizeof(value) + sizeof("\n\0");
-
 
 		memcached_server_free(servers);
 		memcached_free(ab_memcache);
@@ -122,14 +127,11 @@ static ngx_int_t ngx_http_api_block_handler(ngx_http_request_t *r) {
         }
     }
 
-		// size = sizeof(r->connection->addr_text.data) + sizeof("\n\0");
-
     b = ngx_create_temp_buf(r->pool, size);
     if (b == NULL) {
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
     }
 
-		// b->last = ngx_sprintf(b->last, "%s\n", r->connection->addr_text.data);
 		b->last = ngx_sprintf(b->last, "%s\n", value);
 
     out.buf = b;
@@ -152,11 +154,14 @@ static ngx_int_t ngx_http_api_block_handler(ngx_http_request_t *r) {
 
 static char *ngx_http_api_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-    ngx_http_core_loc_conf_t *clcf;
-
-    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    clcf->handler = ngx_http_api_block_handler;
-
     return NGX_CONF_OK;
+}
+
+static ngx_int_t
+ngx_http_api_block_init(ngx_conf_t *cf) {
+		ngx_http_next_body_filter = ngx_http_top_body_filter;
+		ngx_http_top_body_filter = ngx_http_api_block_body_filter;
+
+		return NGX_OK;
 }
 
